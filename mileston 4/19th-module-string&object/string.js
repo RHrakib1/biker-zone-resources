887f7faf54d4bffd3daf88acdc03c906479f22f7
RHrakib1 <rakibhasanmd457@gmail.com>
2024-02-02 17:54:51 +0600

    add to the string in first github

---
+++ mileston 4/19th-module-string&object/string.js
@@ -0,0 +1,12 @@
+// bivinno vabe string declear korar niyom
+const collage="KPI";
+const name='RAKIB';
+const thana=`kurigram`;
+
+
+// array er moto string k change kora jai na
+// koita jaiga nise seta bujhar jonno 
+// koto number index a kon latter ase ta khojar jonno 
+const capital ="gai bandha";
+console.log(capital.length);
+console.log(capital[8]);
--- mileston 4/19th-module-string&object/test.js
+++
@@ -1,4 +0,0 @@
-const arr=["apple", "orange", "banana", "coconat", "jecfurit", "painaapple"]
-for(let i=0; i<=5; i++){
-    console.log(arr[i]);
-}
