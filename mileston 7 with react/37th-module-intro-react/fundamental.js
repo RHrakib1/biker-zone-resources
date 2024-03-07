c483e256123a1cc86086d2f5aa4547a2eb0b17a8
RHrakib1 <rakibhasanmd457@gmail.com>
2024-03-07 10:08:05 +0600

    github

---
+++ mileston 7 with react/37th-module-intro-react/fundamental.js
@@ -0,0 +1,21 @@
+//step-1 : kono akta variable er man jodi porobortite change na kori tahole const use krte hbe
+const milti = 22;
+
+// kono akta variable er man k jodi porobortite change kora hoi tahole let use kora hoi
+let sum = 0;
+sum = 2;
+
+// step-2 : 6 basic condition(<, >, ===, ==!, =<, =>) 
+// multipol condition : &&, ||
+
+
+// step-3 : array 
+// array declear
+const array = [55, 11, 22, 44, 99];
+
+// array er index diye man change kora|| push, pop etc
+array[0] = 22;
+console.log(array);
+// koto gula index ase segula dekha jabe 
+console.log(array.length);
+
